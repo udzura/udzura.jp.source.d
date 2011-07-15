e0ee11ceaa01c57a944e16d3beb3138565bfe3fb
udzura <udzura@udzura.jp>
2011-07-15 19:17:14 +0900

    整理整頓

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+gem 'highline'
+gem 'nanoc3'
+
+gem 'haml'
+gem 'kramdown'
+gem 'simple-rss'
--- lib/default.rb
+++ lib/default.rb
@@ -1,7 +1,12 @@
+# coding: utf-8
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 
 require 'rubygems'
+require 'bundler'
+Bundler.require
+
+require 'open-uri'
 
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Blogging
@@ -11,4 +16,12 @@ include Nanoc3::Helpers::Filtering
 
 def basename(item)
   File.basename(item.identifier)
+end
+
+def udzura_rss
+  @rss ||= SimpleRSS.parse(open('http://blog.udzura.jp/feed/'))
+end
+
+def strip_short(title, len=32)
+  title.split(//)[0..(len-1)].join + "..."
 end
